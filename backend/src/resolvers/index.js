9f32b4bbf420a1f12cca60946a55f30407328765
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2024-06-27 23:08:16 +0800

    Setup transaction resolver and typedef

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -1,7 +1,8 @@
 import { mergeResolvers } from "@graphql-tools/merge";
 
 import userResolver from "./user.resolver.js";
+import transactionResolver from "./transaction.resolver.js";
 
-const mergedResolvers = mergeResolvers([userResolver]);
+const mergedResolvers = mergeResolvers([userResolver, transactionResolver]);
 
 export default mergedResolvers;
---
+++ backend/src/typeDefs/transaction.typeDef.js
@@ -0,0 +1,3 @@
+const transactionTypeDef = `#graphql`;
+
+export default transactionTypeDef;
